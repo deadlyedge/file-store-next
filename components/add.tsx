e4a50cb7f42ea2e9c4572be9bff22f6763f79111
xdream oldlu <xdream@gmail.com>
2024-01-05 23:44:21 +0800

    ui init.

---
+++ components/add.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { FileDrop } from "react-file-drop"
+
+// import { upload_file } from "@/lib/api"
+// import { delay } from "@/lib/utils"
+
+export function Add() {
+  // const handleDropped = (droppedFiles: FileList) => {
+  //   upload_file(droppedFiles)
+  //     .then(() => delay(2000))
+  //     .then(() => window.location.reload())
+  // }
+  const handleDropped = (droppedFiles: FileList) => {
+    droppedFiles
+  }
+
+  return (
+    <div className='z-50 w-40 h-40 border-2 border-dashed text-zinc-800 bg-gray-100 rounded bg-opacity-50 cursor-pointer group hover:bg-opacity-90 hover:z-20 duration-200'>
+      <FileDrop
+        className='w-40 h-40 py-2 pr-2 flex flex-col items-center justify-center'
+        onDrop={(files: FileList | null) => handleDropped(files!)}>
+        <p className='text-center text-lg uppercase'>Drop Files Here!</p>
+        <svg
+          className='w-8 h-8 mx-auto my-3 rotate-45 text-blue-500 group-hover:rotate-[135deg] group-hover:text-lime-500 duration-200'
+          fill='currentColor'
+          viewBox='7 2 10 20'
+          xmlns='http://www.w3.org/2000/svg'>
+          <path d='M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z'></path>
+        </svg>
+        <p className='text-center text-xs mb-2'>
+          Refresh page for the new list of files after upload.
+        </p>
+      </FileDrop>
+    </div>
+  )
+}
---
+++ components/header.tsx
@@ -0,0 +1,34 @@
+import { Whisper } from "next/font/google"
+
+import { Add } from "./add"
+import { cn } from "@/lib/utils"
+
+const whisper = Whisper({ subsets: ["latin"], weight: "400" })
+
+export function Header() {
+  return (
+    <div>
+      <div className='fixed left-0 top-0 z-50 flex bg-gray-800 bg-opacity-70'>
+        <Add />
+      </div>
+      <div>
+        <div className='w-52 h-32 right-0 top-0 fixed bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 background-animate bg-opacity-20'></div>
+        <div className='w-80 h-20 right-0 top-0 fixed bg-gray-50 bg-opacity-20'></div>
+        <div className='w-40 h-40 right-0 top-0 fixed bg-gray-50 bg-opacity-20'></div>
+        <section
+          className={cn(
+            whisper.className,
+            "fixed z-10 text-gray-200 h-40 w-48 top-0 right-0"
+          )}>
+          <div className='absolute bottom-6 -left-8 -rotate-90 text-[3rem] '>
+            mongo
+          </div>
+          <div className='absolute inset-y-0 right-2'>
+            <div className='text-[5rem] -mt-8'>FileStore</div>
+            <div className='font-sans text-xl mt-0 float-right'>NexTS</div>
+          </div>
+        </section>
+      </div>
+    </div>
+  )
+}
