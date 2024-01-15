041e45839b4d8cc73e059da6707520d3b84a77e7
xdream oldlu <xdream@gmail.com>
2024-01-15 11:05:27 +0800

    add default value to mongo connection, fix docker build error

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,10 +1,11 @@
 import { GridFSBucket, MongoClient } from "mongodb"
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
-}
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
+// }
 
-const uri = process.env.MONGODB_URI
+// add default value to prevent docker build error with mongodb
+const uri = process.env.MONGODB_URI || 'mongodb://192.168.50.17/'
 const DEFAULT_DB_NAME = process.env.MONGO_DB_NAME as string
 
 const options = {}
@@ -34,10 +35,10 @@ if (process.env.NODE_ENV === "development") {
 // separate module, the client can be shared across functions.
 export default clientPromise
 
-export const connectToBucket = async (dbName: string | null) => {
+export const connectToBucket = async (dbName: string = DEFAULT_DB_NAME) => {
   const client = await clientPromise
 
-  const changeDotInDbName = dbName?.replace(".", "_") || DEFAULT_DB_NAME
+  const changeDotInDbName = dbName.replace(".", "_")
 
   const bucket = new GridFSBucket(client.db(changeDotInDbName))
 
